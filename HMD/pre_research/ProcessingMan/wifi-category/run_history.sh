b647d18b6220a18e7e914486fcf45b9e581d0e6e
gamency <studenthegong@163.com>
2019-01-09 22:06:51 +0800

    update

--- HMD/pre_research/ProcessingMan/wifi-category/run_history.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nohup /usr/install/spark2-yarn/bin/spark-submit \
---conf spark.ui.port=8259 \
---conf spark.sql.autoBroadcastJoinThreshold=-1 \
---conf spark.driver.maxResultSize=8g \
---driver-memory 8g \
---conf  spark.sql.shuffle.partitions=6000 \
---conf  spark.sql.adaptive.enabled=false \
-$1 $2 > "logs/$1_${datetime}_$2" &
